6a77784cadee96725496e627c3b46f528f5669a1
Toan <hongtoan20042000@gmail.com>
2023-11-13 14:18:17 +0700

    delete file

--- order-service/src/main/java/com/example/orderservicems/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.orderservicems.repository;
-
-import com.example.orderservicems.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order,Long> {
-}

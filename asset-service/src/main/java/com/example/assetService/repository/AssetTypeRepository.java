c7a7fc7ebf36f3e204fec5acfd9088ce3cf92557
Ngoc Tin <ngoctin2412@gmail.com>
2023-11-27 15:51:27 +0700

    add AssetHistory Response

--- asset-service/src/main/java/com/example/assetService/repository/AssetTypeRepository.java
+++ asset-service/src/main/java/com/example/assetService/repository/AssetTypeRepository.java
@@ -3,5 +3,8 @@ package com.example.assetService.repository;
 import com.example.assetService.model.AssetType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AssetTypeRepository extends JpaRepository<AssetType,Long> {
+    List<AssetType> findByAssetType(Long assetType);
 }
